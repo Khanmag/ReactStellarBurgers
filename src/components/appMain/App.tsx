f089a8f73a57b35051b3d943ad1820690f1aec33
Khanmag <Khanmag329@gmail.com>
2022-04-27 13:16:32 +0300

    refactor fetch

--- src/components/appMain/App.tsx
+++ src/components/appMain/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-// import data from "../../utils/data";
-import style from './App.module.css';
-import {ConstructorElement} from '@ya.praktikum/react-developer-burger-ui-components'
 import AppHeader from "../appHeader/AppHeader";
 import MainContainer from "./MainContainer";
-import PropTypes from "prop-types";
 
 const mainURL = 'https://norma.nomoreparties.space/api/ingredients'
 
@@ -14,27 +10,25 @@ const App = () => {
     const [state, setState] = React.useState({
         isLoading: true,
         hasError: false,
-        errorName: '',
         data: [],
     });
 
     React.useEffect(() => {
         setState({...state, isLoading: true});
-
-        const getIngredientData = async () => {
-            const res = await fetch(mainURL);
-            if (res.ok) {
-                const data = await res.json();
+        fetch(mainURL)
+            .then(response => {
+                if (response.ok) return response.json()
+            })
+            .then( data => {
                 setState({...state, isLoading: false, hasError: false, data: data.data})
-            }
-        }
-        getIngredientData().catch(err => {
-            setState({...state, errorName: err, hasError: true, isLoading: false})
-        });
+            })
+            .catch(err => {
+                setState({...state, hasError: true, isLoading: false})
+            })
     }, [])
 
     if (state.isLoading) return <p>Загрузка...</p>
-    else if (state.hasError) return <p>Произошла ошибка {state.errorName}, пожалуйста попробуйте снова</p>
+    else if (state.hasError) return <p>Произошла ошибка, пожалуйста попробуйте снова</p>
 
     return (
         <>
